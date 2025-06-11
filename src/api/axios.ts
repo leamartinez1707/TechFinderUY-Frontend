846ccfaa6dcafa223290c85d8ebfd1260f9ea230
Leandro Martínez <leandro.martinez17@outlook.com>
2025-06-11 20:55:44 -0300

    fix: axios token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 
 const api = axios.create({
@@ -11,7 +12,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token');
+        const token = Cookies.get('refresh_token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
